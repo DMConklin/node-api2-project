4c93b28c54bc826a9bbc8017d74c92e0705e3b48
Darrell-Conklin <dmconklin84@gmail.com>
2020-06-09 21:23:25 -0500

    added end point for GET api/posts/:id/comments and POST api/posts

--- posts/posts-router.js
+++ posts/posts-router.js
@@ -34,4 +34,47 @@ router.get('/api/posts/:id', (req,res) => {
         })
 })
 
+router.get('api/posts/:id/comments', (req,res) => {
+    findById(req.params.id)
+        .then(post => {
+            if (!post) {
+                res.status(404).json({
+                    message: "Post not found"
+                })
+            } else {
+                return findPostComments(req.params.id)
+            }
+        })
+        .then(comments => {
+            res.json(comments)
+        })
+        .catch(err => {
+            console.log(err)
+            res.status(500).json({
+                message: "There was an error fetching comments for this post"
+            })
+        })
+})
+
+router.post('/api/posts', (req,res) => {
+    if (!req.body.title || !req.body.contents) {
+        return res.status(400).json({
+            message: "Posts must contain a title and contents"
+        })
+    }
+
+    posts.insert(req.body)
+        .then(() => {
+            res.status(201).json({
+                message: "Posted successfully"
+            })
+        })
+        .catch(err => {
+            console.log(err)
+            res.status(500).json({
+                message: "There was an error posting your post"
+            })
+        })
+})
+
 module.exports = router
